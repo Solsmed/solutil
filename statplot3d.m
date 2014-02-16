89484977ef8f5bcfc0cde215788cbb02a9218f57
Alexander Solsmed <mail@solsmed.se>
2014-02-16 18:47:03 +0100

    Added faint wireframe lines

--- statplot3d.m
+++ statplot3d.m
@@ -5,7 +5,7 @@ function [ dataVectorQuantiles ] = statplot3d(x, y, data)
 
 statDim = 3;
 quants = [0.25 0.5 0.75];
-alphas = [0.2 0.9 0.2];%0.5 - abs(0.5 - quants);
+alphas = [0.3 0.9 0.3];%0.5 - abs(0.5 - quants);
 markers = ['v','o','^'];
 
 %dataVectorMean = nanmean(data,statDim);
@@ -13,9 +13,9 @@ dataVectorQuantiles = quantile(data,quants,statDim);
 [X Y] = meshgrid(x, y);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% QUANTILE SURFACES
-
+edgeColors = {'none','black','none'};
 for q=1:length(quants)
-    s=surf(x,y,dataVectorQuantiles(:,:,q)','edgecolor','none');
+    s=surf(x,y,dataVectorQuantiles(:,:,q)','EdgeColor',edgeColors{q});
     hold on
     set(s,'FaceAlpha',alphas(q));
 end
@@ -23,16 +23,17 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% QUANTILE RODS
 
 % TODO: shaded rods
-
+%{
 for xx = 1:length(x)
     for yy = 1:length(y)
-        plot3([x(xx) x(xx)],[y(yy) y(yy)],[dataVectorQuantiles(xx,yy,end) dataVectorQuantiles(xx,yy,1)],'k-','LineWidth',2)
+        plot3([x(xx) x(xx)],[y(yy) y(yy)],[dataVectorQuantiles(xx,yy,end) dataVectorQuantiles(xx,yy,1)],'k-','LineWidth',2,'EdgeColor','interp')
     end
 end
 
 for q = 1:length(quants)
     plot3(X,Y,dataVectorQuantiles(:,:,q)',markers(q))
 end
+%}
 
 end
 
